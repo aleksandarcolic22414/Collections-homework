a46809dc2d166b3aafeb9c1398bd75aea07f4378
aleksandarcolic22414 <acacolic@gmail.com>
2016-06-10 18:20:37 +0200

    Izvrsena je provera svih stringova
    Onemoguceno je unosenje praznog string-a u pocetnoj fazi kao i unosenje
    praznog string-a cije naredna dva slova treba predvideti.Takodje je
    izvrsena provera mape kako bi se onemogucilo predvidjanje slova ukoliko
    ne postoji odgovarajuci bigram(cije je pocetno slovo slovo za
    predvidjanje).

--- CollectionsHomework/src/bigramPackage/Bigram.java
+++ CollectionsHomework/src/bigramPackage/Bigram.java
@@ -13,6 +13,10 @@ public class Bigram {
 //		Scanner in = new Scanner(System.in);
 		String recenica = "abbcceeeeeeabcc" ;
 //		recenica = in.nextLine();
+		if(recenica.length() == 0){
+			System.out.println("Unet je prazan tekst za predvidjanje");
+			return;
+		}
 		recenica = recenica.replace(" ", "");
 		
 		Map<String, Integer> mapa = new HashMap<>();
@@ -33,33 +37,48 @@ public class Bigram {
 		}
 		
 		String stringZaPredvidjanje = "ja";
-		char pocetnoSlovoZaPrevidjanje = 
-				stringZaPredvidjanje.charAt(
+		char pocetnoSlovoZaPrevidjanje = 0;
+		try{
+			if(stringZaPredvidjanje.length() != 0)
+				pocetnoSlovoZaPrevidjanje = 
+					stringZaPredvidjanje.charAt(
 							(stringZaPredvidjanje.length() - 1));
+			
+			else throw new RuntimeException("Unet je prazan String");
+		}catch (Exception e){
+			System.out.println(e.getMessage());
+			return;
+		}
+		
 		String predvidjeniTekst = "";
 		
 		int brojSlovaZaPredvidjanje = 2;
-		
-		for(int j = 0; j < brojSlovaZaPredvidjanje; j++){
-			i = mapa.entrySet().iterator();
-			int frekvencija = 0;
-			char predvidjenoSlovo = 0;
-			
-			while(i.hasNext()){
-				Entry<String, Integer> e = (Entry<String, Integer>)i.next();
-				if(e.getKey().charAt(0) == pocetnoSlovoZaPrevidjanje){
-					if(e.getValue() > frekvencija){
-						predvidjenoSlovo = e.getKey().charAt(1);
-						frekvencija = e.getValue();
+		try {
+			for(int j = 0; j < brojSlovaZaPredvidjanje; j++){
+				i = mapa.entrySet().iterator();
+				int frekvencija = 0;
+				char predvidjenoSlovo = 0;
+				
+				while(i.hasNext()){
+					Entry<String, Integer> e = (Entry<String, Integer>)i.next();
+					if(e.getKey().charAt(0) == pocetnoSlovoZaPrevidjanje){
+						if(e.getValue() > frekvencija){
+							predvidjenoSlovo = e.getKey().charAt(1);
+							frekvencija = e.getValue();
+						}
 					}
 				}
+				if(predvidjenoSlovo != 0){
+					predvidjeniTekst += predvidjenoSlovo;
+					pocetnoSlovoZaPrevidjanje = predvidjenoSlovo;
+				}else throw new RuntimeException("Ne postoji bigram koji sadrzi dato slovo");
 			}
-			if(predvidjenoSlovo != 0){
-				predvidjeniTekst += predvidjenoSlovo;
-				pocetnoSlovoZaPrevidjanje = predvidjenoSlovo;
-			}else throw new RuntimeException("Ne postoji bigram koji sadrzi dato slovo");
+		} catch (Exception e){
+			System.out.println(e.getMessage());
 		}
-		System.out.println(predvidjeniTekst);
+		System.out.println("Naredna dva slova su: " + predvidjeniTekst);
+		System.out.println("Predvidjeni string: " + stringZaPredvidjanje + predvidjeniTekst);
+	
 	}
 
 }

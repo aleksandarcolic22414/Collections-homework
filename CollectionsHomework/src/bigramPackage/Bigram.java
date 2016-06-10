ee68d80d5f1bfc94dd37e58bba8b13d6a0301414
aleksandarcolic22414 <acacolic@gmail.com>
2016-06-10 18:05:01 +0200

    Napravljena funkcionalnost predvidjanja
    Dodata funkcija polazi od krajnjeg slova unetog string-a i na osnovu
    frekvencija pojavljivanja bigrama koji pocinju da dato slovo predvidja
    naredno slovo.Moze se uneti proizvoljan broj slova za predvidjanje.

--- CollectionsHomework/src/bigramPackage/Bigram.java
+++ CollectionsHomework/src/bigramPackage/Bigram.java
@@ -3,14 +3,16 @@ package bigramPackage;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class Bigram {
 
 	public static void main(String[] args) {
 		
-		Scanner in = new Scanner(System.in);
-		String recenica = in.nextLine();
+//		Scanner in = new Scanner(System.in);
+		String recenica = "abbcceeeeeeabcc" ;
+//		recenica = in.nextLine();
 		recenica = recenica.replace(" ", "");
 		
 		Map<String, Integer> mapa = new HashMap<>();
@@ -30,6 +32,34 @@ public class Bigram {
 			System.out.println(i.next());
 		}
 		
+		String stringZaPredvidjanje = "ja";
+		char pocetnoSlovoZaPrevidjanje = 
+				stringZaPredvidjanje.charAt(
+							(stringZaPredvidjanje.length() - 1));
+		String predvidjeniTekst = "";
+		
+		int brojSlovaZaPredvidjanje = 2;
+		
+		for(int j = 0; j < brojSlovaZaPredvidjanje; j++){
+			i = mapa.entrySet().iterator();
+			int frekvencija = 0;
+			char predvidjenoSlovo = 0;
+			
+			while(i.hasNext()){
+				Entry<String, Integer> e = (Entry<String, Integer>)i.next();
+				if(e.getKey().charAt(0) == pocetnoSlovoZaPrevidjanje){
+					if(e.getValue() > frekvencija){
+						predvidjenoSlovo = e.getKey().charAt(1);
+						frekvencija = e.getValue();
+					}
+				}
+			}
+			if(predvidjenoSlovo != 0){
+				predvidjeniTekst += predvidjenoSlovo;
+				pocetnoSlovoZaPrevidjanje = predvidjenoSlovo;
+			}else throw new RuntimeException("Ne postoji bigram koji sadrzi dato slovo");
+		}
+		System.out.println(predvidjeniTekst);
 	}
 
 }
